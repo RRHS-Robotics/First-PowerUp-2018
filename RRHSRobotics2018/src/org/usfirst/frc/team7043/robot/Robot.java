fcdc1eb3c23770b8ff5855d689b19210426ba35a
Austin Wilson <austinwilson@Austins-MacBook-Pro.local>
2018-02-12 15:43:23 -0500

    added intake and pully logic

--- RRHSRobotics2018/src/org/usfirst/frc/team7043/robot/Robot.java
+++ RRHSRobotics2018/src/org/usfirst/frc/team7043/robot/Robot.java
@@ -15,8 +15,13 @@ import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 import org.usfirst.frc.team7043.robot.commands.AutoRightDriveCommand;
+import org.usfirst.frc.team7043.robot.commands.IntakePullCommand;
+import org.usfirst.frc.team7043.robot.commands.IntakeReleaseCommand;
+import org.usfirst.frc.team7043.robot.commands.LowerIntakeCommand;
+import org.usfirst.frc.team7043.robot.commands.RaiseIntakeCommand;
 import org.usfirst.frc.team7043.robot.commands.TankDriveCommand;
 import org.usfirst.frc.team7043.robot.subsystems.DriveTrainSubsystem;
+import org.usfirst.frc.team7043.robot.subsystems.IntakeSubsystem;
 
 /**
  * The VM is configured to automatically run this class, and to call the
@@ -27,9 +32,12 @@ import org.usfirst.frc.team7043.robot.subsystems.DriveTrainSubsystem;
  */
 public class Robot extends TimedRobot {
 	public static final DriveTrainSubsystem DriveTrain = new DriveTrainSubsystem();
+	public static final IntakeSubsystem Intake = new IntakeSubsystem();
 	public static OI refOI = new OI();
 	public static RobotMap robotMap = new RobotMap();
 
+	
+	
 	Command driveTrainCommand = new TankDriveCommand();
 	
 	Command selectedAutonomousCommand;
@@ -44,10 +52,14 @@ public class Robot extends TimedRobot {
 		autoChooser.addDefault("Right Auto Drive", new AutoRightDriveCommand());
 		//autoChooser.addDefault("Middle Auto Drive", new AutoMiddleDriveCommand());
 		//autoChooser.addDefault("Left Auto Drive", new AutoLeftDriveCommand());
-		// chooser.addObject("My Auto", new MyAutoCommand());
+		//chooser.addObject("My Auto", new MyAutoCommand());
 		SmartDashboard.putData("Auto mode", autoChooser);
 		RobotMap.leftDrive.setInverted(true);
 		RobotMap.robotDriveMain = new DifferentialDrive(RobotMap.leftDrive, RobotMap.rightDrive);
+		refOI.triggerLeft.whileHeld(new IntakePullCommand());
+		refOI.triggerRight.whileHeld(new IntakeReleaseCommand());
+		refOI.raiseIntake.whileHeld(new RaiseIntakeCommand());
+		refOI.lowerIntake.whileHeld(new LowerIntakeCommand());
 	}
 
 	/**
--- RRHSRobotics2018/src/org/usfirst/frc/team7043/robot/RobotMap.java
+++ RRHSRobotics2018/src/org/usfirst/frc/team7043/robot/RobotMap.java
@@ -40,4 +40,16 @@ public class RobotMap {
 	
 	public static SpeedControllerGroup rollerIntake = new SpeedControllerGroup(leftIntakeMotor,rightIntakeMotor);
 	//END Intake Motor Map
+	
+	//Start Pulley System Map
+	public static Spark pulleyMotor = new Spark(2); //PWM Pin group 2 on RoboRio
+	//End Pulley System Map
+	
+	//Start Insulting Children
+	//Kylie is a loser
+	//Eric is only here to model stuff
+	//Will is not actually swedish
+	//End Insulting Children
+	
+	
 }
